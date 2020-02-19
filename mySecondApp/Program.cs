02ec4ef9a32f68e6fb02d5dc8c06f6fdbde7ce14
Dj <ritchey0713@gmail.com>
2020-02-19 00:56:56 -0500

    add floats

--- mySecondApp/Program.cs
+++ mySecondApp/Program.cs
@@ -8,11 +8,12 @@ namespace mySecondApp
         {
             int num1 = 10;
             int num2 = 20;
+            int num3, num4, num5;
             int sum = num1 + num2;
             double d1 = 3.5;
             double d2 = 1.5;
             double sum2 = d1 / d2;
-            // when dividing use double cant convert floats to int
+            // when dividing use double cant convert floats to int or use Casting
 
             Console.WriteLine("the sum of " + d1 + " and " + d2 + " = " + sum2);
         }
