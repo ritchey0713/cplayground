bfbd499044122d619d951bad41f29bb52e930bae
Dj <ritchey0713@gmail.com>
2020-02-18 23:17:04 -0500

    first

---
+++ mySecondApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mySecondApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("fuck you");
+        }
+    }
+}
