2dec32f817384aa85e4a5492e68117109169a766
Dj <ritchey0713@gmail.com>
2020-02-19 00:57:03 -0500

    add strings

--- mySecondApp/Program.cs
+++ mySecondApp/Program.cs
@@ -10,12 +10,20 @@ namespace mySecondApp
             int num2 = 20;
             int num3, num4, num5;
             int sum = num1 + num2;
+            
             double d1 = 3.5;
             double d2 = 1.5;
             double sum2 = d1 / d2;
             // when dividing use double cant convert floats to int or use Casting
 
-            Console.WriteLine("the sum of " + d1 + " and " + d2 + " = " + sum2);
+            float f1 = 3.5f;
+            float f2 = 10.5f;
+
+            string myName = "DJ";
+            string message = "My name is ";
+
+
+            Console.WriteLine(message + myName);
         }
     }
 }
