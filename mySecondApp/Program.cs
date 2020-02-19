cbdbbf4a4e6232c197d2811972ba5215dd31abd4
Dj <ritchey0713@gmail.com>
2020-02-19 00:51:54 -0500

    add notes

--- mySecondApp/Program.cs
+++ mySecondApp/Program.cs
@@ -9,7 +9,12 @@ namespace mySecondApp
             int num1 = 10;
             int num2 = 20;
             int sum = num1 + num2;
-            Console.WriteLine(sum);
+            double d1 = 3.5;
+            double d2 = 1.5;
+            double sum2 = d1 / d2;
+            // when dividing use double cant convert floats to int
+
+            Console.WriteLine("the sum of " + d1 + " and " + d2 + " = " + sum2);
         }
     }
 }
