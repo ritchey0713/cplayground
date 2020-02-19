c0037f16cc3358c415baa1ef11d25c7a747f8e97
Dj <ritchey0713@gmail.com>
2020-02-19 00:47:49 -0500

    add sums

--- mySecondApp/Program.cs
+++ mySecondApp/Program.cs
@@ -6,7 +6,10 @@ namespace mySecondApp
     {
         static void Main(string[] args)
         {
-
+            int num1 = 10;
+            int num2 = 20;
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
         }
     }
 }
